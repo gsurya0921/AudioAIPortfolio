3b797cb559d8ac8a943af82922f056b090b9ff84
GS0921 <ganeshsurya0921@gmail.com>
2021-09-29 11:47:18 +0530

    We changed hi to hello, added new question

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("how are you")
-    print("foo")
+    print("fooq")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
